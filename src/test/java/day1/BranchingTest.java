75e5fcd88e7cb8442233fe22c617effc2868a0f5
Neha <singhneha0024@gmail.com>
2021-10-12 15:26:14 +0530

    added files in day1

---
+++ src/test/java/day1/BranchingTest.java
@@ -0,0 +1,15 @@
+package day1;
+
+import org.testng.annotations.Test;
+
+public class BranchingTest {
+
+  @Test
+  public void mainTest() {
+	  System.out.println("Hello im 1");  }
+  
+  @Test
+  public void mainTest2() {
+    System.out.println("Hello im 2");
+  }
+}
